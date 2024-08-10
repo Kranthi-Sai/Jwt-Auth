95840a76f1fc984a9cc29d7c367bb787f6b8393a
Kranthi Sai <kranthisai@Kranthis-MacBook-Pro.local>
2024-08-10 13:44:53 +0530

    fix: connecting to database and load env usingGetEnv

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"net/http"
+	"Jwt-Auth/database"
+	"Jwt-Auth/initializer"
+	
 
   "github.com/gin-gonic/gin"
 )
 
 func main() {
 	r:=gin.Default()
+	initializer.LoadEnvVariables()
+	_,err:= database.ConnectDB()
+	if err!=nil{
+		panic("Database not Connected")
+	}
 	r.GET("/register",func(c *gin.Context){
 		c.JSON(http.StatusOK,gin.H{
 			"message":"register SuccessFully",
